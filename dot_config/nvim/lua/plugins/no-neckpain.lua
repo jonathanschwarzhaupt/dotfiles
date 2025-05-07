ef7bcb48fe548416745e816807201aafded15be1
jonathanschwarzhaupt <jonathan.schwarzhaupt@gmail.com>
2025-05-07 08:43:41 +0000

    add no neck pain

---
+++ dot_config/nvim/lua/plugins/no-neckpain.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+    cmd = "NoNeckPain"
+    keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
+    opts = {},
+
+  },
+}
